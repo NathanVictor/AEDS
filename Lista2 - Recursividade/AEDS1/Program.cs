17cb16ff5d55420b4f0732d4896401883b969b9c
Nathan3345 <nathan3345.victor@gmail.com>
2025-02-27 01:29:46 -0300

    feat: update Recursividade

---
+++ Lista2 - Recursividade/AEDS1/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static int Recursividade(int a, int n)
+    {
+        int resp;
+
+        if (n == 0)
+        {
+            resp = 1;
+        }
+        else
+        {
+            resp = a * Recursividade(a, n - 1);
+        }
+
+        return resp;
+    }
+
+    public static void Main()
+    {
+        int valor = Recursividade(2, 3);
+        Console.WriteLine(valor);
+    }
+}
---
+++ Lista2 - Recursividade/AEDS2/Program.cs
@@ -0,0 +1,23 @@
+﻿class Program
+{
+    public static int Valor(int m, int n){
+
+        int resp;
+
+        if(m == n){
+            resp = m;
+
+        }else{
+            resp = m + Valor(m + 1, n);
+        }
+
+        return resp;
+    }
+    public static void Main()
+    {
+        int valor = Valor(0,3);
+
+        Console.WriteLine(valor);
+    
+    }
+}
