0e0c4f657d679e8f550b5f6989ea5b98fec359b4
Nathan3345 <nathan3345.victor@gmail.com>
2025-02-28 00:50:58 -0300

    feat: Update 3 exercice

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿class Program
+{
+    public static void async int Valor(double a)
+    {
+        int resp;
+
+        if (a == 1)
+        {
+            resp = 1
+        }
+        else
+        {
+
+            resp = Valor(2*);
+
+        }
+
+
+
+
+    }
+    public static void Main()
+    {
+
+    }
+}
