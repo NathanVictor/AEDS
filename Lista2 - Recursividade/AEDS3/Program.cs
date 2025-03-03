a5b6752fd351cb70a5fd38167001e6621f0a60b0
Nathan3345 <nathan3345.victor@gmail.com>
2025-03-03 13:59:59 -0300

    feat: Update Exercice 3 and Creation Exercice 4

---
+++ Lista2 - Recursividade/AEDS3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+class Program
+{
+    public static void converterBinario(int n)
+    {
+
+        if (n == 1)
+        {
+            Console.Write(1);
+        }
+        else
+        {
+            converterBinario(n / 2);
+            Console.Write(n % 2);
+
+        }
+    }
+    public static void Main()
+    {
+        int valor = 12;
+        converterBinario(valor);
+
+    }
+}
