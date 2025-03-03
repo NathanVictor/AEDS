6e52fad504698ac039af7e69a26049d53516aaeb
Nathan3345 <nathan3345.victor@gmail.com>
2025-03-03 14:35:22 -0300

    feat: update creation exercice 4

--- Lista2 - Recursividade/AEDS4/Program.cs
+++ Lista2 - Recursividade/AEDS4/Program.cs
@@ -2,8 +2,30 @@
 
 class Program
 {
+    public static int Maximo(int x, int y)
+    {
+
+        if (x >= y)
+        {
+            return = x;
+
+        }
+        else
+        {
+            return = Maximo(x - y, y);
+        }else
+        {
+            return Maximo(y, x)
+        }
+
+    }
+
+
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int valor1 = 10;
+        int valor2 = 6;
+
+        Console.WriteLine(Maximo(valor1, valor2));
     }
 }
