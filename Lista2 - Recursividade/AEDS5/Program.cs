a48d66536a4ebb6c75cb18218204cfaa2f026ae9
Nathan3345 <nathan3345.victor@gmail.com>
2025-03-03 22:21:03 -0300

    feat: update creation Exercice 5

---
+++ Lista2 - Recursividade/AEDS5/Program.cs
@@ -0,0 +1,27 @@
+﻿class Program
+{
+    public static int Menor(int[] x, int n)
+    {
+        
+        if (n == 0)
+        {
+            return 0;
+        }
+        else
+        {
+            
+            int count = (x[n - 1] < 0) * 1; 
+
+           
+            return count + Menor(x, n - 1);
+        }
+    }
+
+    public static void Main(string[] args)
+    {
+        int[] array = {1, 2, 3, 4, 5};
+        int n = array.Length;
+
+        Console.WriteLine(Menor(array, n));  
+    }
+}
