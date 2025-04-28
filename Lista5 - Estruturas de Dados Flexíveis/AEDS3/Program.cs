78f5d98cfcfb5eeba16d5a34a379e5d222963edc
Nathan Victor Morais Nogueira <nathan3345.victor@gmail.com>
2025-04-28 00:45:26 -0300

    feat: update code

--- Lista5 - Estruturas de Dados Flexíveis/AEDS3/Program.cs
+++ Lista5 - Estruturas de Dados Flexíveis/AEDS3/Program.cs
@@ -1,65 +1,119 @@
-﻿class Celula
-{
-    private int Elemento { get; set; }
-    private Celula prox { get; set; }
+﻿using System;
 
-    public Celula(int elemento)
+class Arquivo
+{
+    public string Nome { get; set; }
+    public int NumeroPaginas { get; set; }
+
+    public Arquivo(string nome, int numeroPaginas)
+    {
+        Nome = nome;
+        NumeroPaginas = numeroPaginas;
+    }
+}
+
+class Celula
+{
+    public Arquivo Elemento { get; set; }
+    public Celula Prox { get; set; }
+
+    public Celula(Arquivo elemento)
     {
         this.Elemento = elemento;
-        this.prox = null;
+        this.Prox = null;
     }
 
     public Celula()
     {
-        this.Elemento = 0;
-        this.prox = null;
+        this.Elemento = null;
+        this.Prox = null;
     }
 }
 
-
 class Fila
 {
-
     private Celula primeiro, ultimo;
-    public Fila() // Cria um Celula
+
+    public Fila()
     {
         primeiro = new Celula();
         ultimo = primeiro;
     }
 
-    public void inserir(int x) // Cria uma Celula, porém com um "elemento" dentro.
+    public void Inserir(Arquivo arquivo)
     {
-        ultimo.Prox = new Celula(x);
+        ultimo.Prox = new Celula(arquivo);
         ultimo = ultimo.Prox;
     }
 
-    public int Remover() // Remove a ultima Celula da fila.
+    public Arquivo Remover()
     {
-
-
-    }
-
-    public void Mostrar() // Mostra a fila completra.
-    {
-        if (primeiro == ultime)
+        if (primeiro == ultimo)
         {
-            throw new Exception("Erro!");
-
+            throw new Exception("Erro: fila vazia!");
         }
-        Celula tmp = primeiro; // Cria uma variável "temporaria" na primeira Celula e "tmp" é quem vai remover da Celula.
-        primeiro = primeiro.prox;
-        int elemento = primeiro.Elemento;
-        tmp.prox = null;
+
+        Celula tmp = primeiro;
+        primeiro = primeiro.Prox;
+        Arquivo elemento = primeiro.Elemento;
+        tmp.Prox = null;
         tmp = null;
         return elemento;
-        
+    }
+
+    public void Mostrar()
+    {
+        for (Celula i = primeiro.Prox; i != null; i = i.Prox)
+        {
+            Console.WriteLine($"{i.Elemento.Nome} {i.Elemento.NumeroPaginas}");
+        }
+    }
+
+    public bool EstaVazia()
+    {
+        return primeiro == ultimo;
     }
 }
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
+        Fila fila = new Fila();
+        int op = 0;
 
+        do
+        {
+            Console.WriteLine("Op:");
+            if (!int.TryParse(Console.ReadLine(), out op))
+            {
+                Console.WriteLine("Digite um número válido!");
+                continue;
+            }
+
+            if (op == 1)
+            {
+                string nome = Console.ReadLine();
+                int paginas = int.Parse(Console.ReadLine());
+                Arquivo novoArquivo = new Arquivo(nome, paginas);
+                fila.Inserir(novoArquivo);
+            }
+            else if (op == 2)
+            {
+                if (!fila.EstaVazia())
+                {
+                    Arquivo impresso = fila.Remover();
+                    Console.WriteLine($"{impresso.Nome}");
+                }
+            }
+            else if (op == 3)
+            {
+                fila.Mostrar();
+            }
+            else if (op == 4)
+            {
+                Console.WriteLine("");
+            }
+        } while (op != 4);
     }
 }
