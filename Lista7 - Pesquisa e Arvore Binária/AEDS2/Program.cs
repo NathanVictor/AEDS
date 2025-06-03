674d3087b7338c007f0e300c3bfddd743cc6a66b
Nathan Victor Morais Nogueira <nathan3345.victor@gmail.com>
2025-06-03 12:38:08 -0300

    feat: update code exercice tree!

---
+++ Lista7 - Pesquisa e Arvore Binária/AEDS2/Program.cs
@@ -0,0 +1,82 @@
+﻿class No
+{
+    private int elemento;
+    private No esq;
+    private No dir;
+
+
+    public No(int elemento)
+    {
+        this.elemento = elemento;
+        esq = null;
+        dir = null;
+    }
+
+    public No(int elemento, No esq, No dir)
+    {
+        this.elemento = elemento;
+        this.esq = esq;
+        this.dir = dir;
+    }
+}
+
+class Program
+{
+
+    private No raiz;
+
+    public ArvoreBinaria()
+    {
+        raiz = null;
+    }
+
+    public void InserirBinario(int x)
+    {
+        raiz = Inserir(x, raiz);
+
+    }
+
+    private No Inserir(int x, No i)
+    {
+
+        if (i == null)
+        {
+            i = new No(x);
+        }
+        else if (x < i.Elemento)
+        {
+            i.esq = Inserir(x, i.esq);
+        }
+        else if (x > i.Elemento)
+        {
+            i.dir = Inserir(x, i.dir);
+        }
+        else
+        {
+            throw new Exception("Erro!");
+        }
+        return i;
+    }
+
+    public static RemoverBinario()
+    {
+
+    }
+
+    public static MostrarMaiorElementoBinario()
+    {
+
+    }
+
+    public static MostrarMenorElementoBinario()
+    {
+
+    }
+
+    public static MostrarTodosElementosArvoreCentral()
+    {
+
+    }
+
+
+}
