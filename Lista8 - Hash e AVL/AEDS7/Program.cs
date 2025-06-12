36fb53e842a7571c9bd47b7c91c6fb49c3303663
Nathan Victor Morais Nogueira <nathan3345.victor@gmail.com>
2025-06-12 18:18:36 -0300

    feat: update code menu

--- Lista8 - Hash e AVL/AEDS7/Program.cs
+++ Lista8 - Hash e AVL/AEDS7/Program.cs
@@ -1,7 +1,51 @@
 ﻿class Program
 {
-    static void Main(string[] args)
+
+    public class TabelaHash()
     {
-        Console.WriteLine("Hello, World!");
+
+
+
+    }
+
+
+
+
+    public static void Main(string[] args)
+    {
+
+        TabelaHash hash = new TabelaHash();
+
+
+        do
+        {
+            Console.WriteLine("Opção:");
+            string sc = Console.ReadLine();
+            opcao = int.Parse(sc);
+
+            string nome;
+
+            switch (opcao)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+
+                case 3:
+                    break;
+
+                case 4:
+                    break;
+
+                case 5:
+                    break;
+
+                default:
+                    break;  
+            }
+
+        } while (opcao != 5);
     }
 }
